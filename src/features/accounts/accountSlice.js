0d29f6c4c7588ea8a729fad6a927ec4388c767a5
stuart-pike <110692951+stuart-pike@users.noreply.github.com>
2024-06-25 12:15:02 -0700

    initial commit. Implemented with react redux, redux middleware andAPI call with redux thunk

---
+++ src/features/accounts/accountSlice.js
@@ -0,0 +1,72 @@
+const initialStateAccount = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+  isLoadinf: false,
+};
+
+export default function accountReducer(state = initialStateAccount, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
+    case "account/withdrawl":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    case "account/convertingCurrency":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    default:
+      return state;
+  }
+}
+
+//Action creator functions
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+  //make the API call here, for this thunk is required. In this action creator, the action is not returned immediately, instead the function below.
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    // API call
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+    console.log(converted);
+
+    // return action
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+}
+
+export function withdrawl(amount) {
+  return { type: "account/withdrawl", payload: amount };
+}
+
+export function requestLoan(amount, purpose) {
+  return { type: "account/requestLoan", payload: { amount, purpose } };
+}
+
+export function payLoan() {
+  return { type: "account/payLoan" };
+}
